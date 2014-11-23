d193c826091273f694638cf35d0c2168e0bca590
anishadhamani <anisa.dhamani@mail.utoronto.ca>
2014-11-23 18:05:12 -0500

    Best six and worst months() added

--- mining.py
+++ mining.py
@@ -2,7 +2,7 @@
 
 """ Docstring """
 
-__author__ = 'Anisa/Grant/Vidhya'
+__author__ = 'Anisha/Grant/Vidhya'
 __email__ = "ses@drsusansim.org"
 
 __copyright__ = "2014 Susan Sim"
@@ -10,58 +10,60 @@ __license__ = "MIT License"
 
 __status__ = "Prototype"
 
-# imports one per line
 import json
 import datetime
+import operator
 
 stock_data = []
 monthly_averages = []
 year_list = []
 month_list = []
 month_tuple = ()
+
+
 def read_stock_data(stock_name, stock_file_name):
-   stock_data = read_json_from_file(stock_file_name)
-   for item in stock_data:
-        date = datetime.datetime.strptime(item["Date"], "%Y-%m-%d")
-        year_month_format = date.strftime('%Y-%m')
+    stock_data = read_json_from_file(stock_file_name)
+    for entry in stock_data:
+        date = datetime.datetime.strptime(entry["Date"], "%Y-%m-%d")
         if date.year not in year_list:
-         year_list.append(date.year)
+            year_list.append(date.year)
         if date.month not in month_list:
-         month_list.append(date.month)
-   print(year_list)
-   print(month_list)
-   for year_value in year_list:
-       for month_value in month_list:
-           month_average = 0
-           product = 0
-           sum_of_volumes = 0
-           month_tuple = ()
-           for item in stock_data:
-               item_date = datetime.datetime.strptime(item["Date"], "%Y-%m-%d")
-               item_year = item_date.year
-               item_month = item_date.month
-               if item_year == year_value:
-                   if item_month == month_value:
-                       product += item["Volume"] * item["Close"]
-                       sum_of_volumes += item["Volume"]
-           if sum_of_volumes != 0:
-               month_average = product/ sum_of_volumes
-               month_average = round(month_average,2)
-               year_month = str(year_value) + "/" + str(month_value)
-               month_tuple = (year_month,month_average)
-               monthly_averages.append(month_tuple)
-               print(month_tuple)
-   print(monthly_averages)
-
-
+            month_list.append(date.month)
+    print(year_list)
+    print(month_list)
+    for year_value in year_list:
+            for month_value in month_list:
+                month_average = 0
+                product = 0
+                sum_of_volumes = 0
+                for item in stock_data:
+                    item_date = datetime.datetime.strptime(item["Date"], "%Y-%m-%d")
+                    item_year = item_date.year
+                    item_month = item_date.month
+                    if item_year == year_value:
+                        if item_month == month_value:
+                            product += item["Volume"] * item["Close"]
+                            sum_of_volumes += item["Volume"]
+                if sum_of_volumes != 0:
+                        month_average = product/ sum_of_volumes
+                        month_average = round(month_average,2)
+                        month_value = '{0:0=2d}'.format(month_value)
+                        year_month = str(year_value) + "/" + str(month_value)
+                        month_tuple = (year_month,month_average)
+                        monthly_averages.append(month_tuple)
 
 
 def six_best_months():
-    return [('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0)]
+    monthly_averages.sort(key=operator.itemgetter(-1))
+    monthly_averages.reverse()
+    print("Best six months: ", monthly_averages[0:6])
+    return monthly_averages[0:6]
 
 
 def six_worst_months():
-    return [('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0)]
+    monthly_averages.sort(key=operator.itemgetter(-1))
+    print("Worst six months: ", monthly_averages[0:6])
+    return monthly_averages[0:6]
 
 
 def read_json_from_file(file_name):
