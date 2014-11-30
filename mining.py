64d4ecce8e3889e1abbd4a1090c4c311cd285065
anishadhamani <anisa.dhamani@mail.utoronto.ca>
2014-11-29 23:25:56 -0500

    Comments and docstrings added

--- mining.py
+++ mining.py
@@ -1,71 +1,116 @@
 #!/usr/bin/env python3
 
-""" Docstring """
+""" Data Mining on the prices of stock data """
 
 __author__ = 'Anisha/Grant/Vidhya'
-__email__ = "ses@drsusansim.org"
 
-__copyright__ = "2014 Susan Sim"
-__license__ = "MIT License"
-
-__status__ = "Prototype"
 
 import json
 import datetime
 import operator
+# Package for plotting graphs
+import matplotlib
+matplotlib.use('TKAgg')
 import matplotlib.pyplot as plt
+# Package for GUI
 import tkinter
 
+#global variables
 stock_data = []
 monthly_averages = []
-year_list = []
-month_list = []
-month_tuple = ()
 
 
 def read_stock_data(stock_name, stock_file_name):
-    stock_data = read_json_from_file(stock_file_name)
-    for entry in stock_data:
+    """
+    Reads the stock data and makes call to get_monthly_averages function
+    :param stock_name: string, possible values : 'GOOG' or 'TSE-SO'
+    :param stock_file_name: The name of a JSON formatted file that contains stock data
+    :return: none
+    """
+    global stock_data
+    # Check if stock name is 'GOOG' or 'TSE-SO'
+    if stock_name == "GOOG":
+        stock_data = read_json_from_file(stock_file_name)
+        get_monthly_averages(stock_data)
+    elif stock_name == "TSE-SO":
+        stock_data = read_json_from_file(stock_file_name)
+        get_monthly_averages(stock_data)
+    else:
+        raise ValueError("Invalid stock name")
+
+
+def get_monthly_averages(stock_info):
+    """
+    Calculates the monthly average prices of a stock
+    :param stock_info: List with information on stock i.e date, open, high, low, close, volume
+    :return: none
+    """
+    monthly_averages[:] = []
+    year_list = []
+    month_list = []
+
+    for entry in stock_info:
+        # Extracting date from stock_info
         date = datetime.datetime.strptime(entry["Date"], "%Y-%m-%d")
         if date.year not in year_list:
             year_list.append(date.year)
         if date.month not in month_list:
             month_list.append(date.month)
     for year_value in year_list:
-            for month_value in month_list:
-                product = 0
-                sum_of_volumes = 0
-                for item in stock_data:
-                    item_date = datetime.datetime.strptime(item["Date"], "%Y-%m-%d")
-                    item_year = item_date.year
-                    item_month = item_date.month
-                    if item_year == year_value:
-                        if item_month == month_value:
-                            product += item["Volume"] * item["Close"]
-                            sum_of_volumes += item["Volume"]
-                if sum_of_volumes != 0:
-                        month_average = product/ sum_of_volumes
-                        month_average = round(month_average, 2)
-                        month_value = '{0:0=2d}'.format(month_value)
-                        year_month = str(year_value) + "/" + str(month_value)
-                        month_tuple = (year_month,month_average)
-                        monthly_averages.append(month_tuple)
+        for month_value in month_list:
+            product = 0
+            sum_of_volumes = 0
+            for item in stock_data:
+                item_date = datetime.datetime.strptime(item["Date"], "%Y-%m-%d")
+                item_year = item_date.year
+                item_month = item_date.month
+                if item_year == year_value:
+                    if item_month == month_value:
+                        # Calculating average price
+                        # Step1 = (V1 * C1 + V2 * C2 + ... + Vn * Cn) where V is volume and C is close price
+                        product += item["Volume"] * item["Close"]
+                        # Step2 = V1 + V2 + .... + Vn
+                        sum_of_volumes += item["Volume"]
+            if sum_of_volumes != 0:
+                    # Step3 = Step1 / Step2
+                    month_average = product/sum_of_volumes
+                    month_average = round(month_average, 2)
+                    # Convert month value to 2 digits; ex - from 2 to 02
+                    month_value = '{0:0=2d}'.format(month_value)
+                    year_month = str(year_value) + "/" + str(month_value)
+                    # Tuple for each month with month/year and average stock price for that month
+                    month_tuple = (year_month, month_average)
+                    # monthly_averages list with appended tuples
+                    monthly_averages.append(month_tuple)
 
 
 def six_best_months():
+    """
+    Determines the best six months based on monthly average stock prices
+    :return: List of tuples, (month/year, monthly average)
+    """
+    # Sort the list monthly_averages
     monthly_averages.sort(key=operator.itemgetter(-1))
+    # Reverse the list to get months with greater monthly averages
     monthly_averages.reverse()
-    print("Best six months: ", monthly_averages[0:6])
     return monthly_averages[0:6]
 
 
 def six_worst_months():
+    """
+    Determines the worst six months based on monthly averages
+    :return: List of tuples, (month/year, monthly average)
+    """
     monthly_averages.sort(key=operator.itemgetter(-1))
-    print("Worst six months: ", monthly_averages[0:6])
     return monthly_averages[0:6]
 
 
 def read_json_from_file(file_name):
+    """
+    Reads data from a json file
+    :param file_name: The name of a JSON formatted file that contains stock data
+    :return: json data loaded and parsed
+    """
     with open(file_name) as file_handle:
         file_contents = file_handle.read()
 
@@ -73,31 +118,32 @@ def read_json_from_file(file_name):
 
 
 def visualize(stock_name):
+    """
+    Creates a visualization (bar graph) for best six and worst six months for the given stock file
+    :param stock_name: string, possible values : 'GOOG' or 'TSE-SO'
+    :return: none
+    """
     six_best = six_best_months()
     six_worst = six_worst_months()
-    print(six_best)
-    print(six_worst)
-
+    # Plot six best months - average monthly stock prices over time
     for i in range(len(six_best)):
-        a, b = six_best[i]
-        some_date = datetime.datetime.strptime(a, '%Y/%m')
-        x_series = some_date
-        y_series = b
-        print(x_series, y_series)
-        plt.bar(some_date, b, align='center', width=10.0, color='green')
+        month_year, average = six_best[i]
+        x_series = datetime.datetime.strptime(month_year, '%Y/%m')
+        y_series = average
+        plt.bar(x_series, y_series, align='center', width=10.0, color='green')
     plt.xticks(rotation=15)
     plt.title("Six Best Months for %s" % stock_name, fontsize=16)
     plt.xlabel("Time", fontsize=14)
     plt.ylabel("Average Monthly Price", fontsize=14)
+    # Save the graph as an image for the given stock name in the current project
     plt.savefig('Six_Best_Months_%s.png' % stock_name)
     plt.show()
+    # Plot six worst months - average monthly stock prices over time
     for i in range(len(six_worst)):
-        c, d = six_worst[i]
-        any_date = datetime.datetime.strptime(c, '%Y/%m')
-        x_series = any_date
-        y_series = d
-        print(x_series, y_series)
-        plt.bar(any_date, d, align='center', width=10.0, color='purple')
+        month_year, average = six_worst[i]
+        x_series = datetime.datetime.strptime(month_year, '%Y/%m')
+        y_series = average
+        plt.bar(x_series, y_series, align='center', width=10.0, color='purple')
     plt.xticks(rotation=15)
     plt.title("Six Worst Months for %s" % stock_name, fontsize=16)
     plt.xlabel("Time", fontsize=14)
@@ -107,6 +153,10 @@ def visualize(stock_name):
 
 
 def gui_function():
+    """
+
+    :return: none
+    """
 
     def invoke_function(event):
         stock_name = entry.get()
@@ -116,10 +166,15 @@ def gui_function():
             var1.set("Calculation of average stock price(monthly) for GOOG Json file Successful! You can proceed!")
             message = tkinter.Message(window, textvariable=var1, bg='green', fg='white')
             message.pack()
-        else:
+        elif stock_name == "TSE-SO":
             read_stock_data("TSE-SO", "data/TSE-SO.json")
             var1 = tkinter.StringVar()
             var1.set("Calculation of average stock price(monthly) for TSE-SO Json file Successful! You can proceed!")
+            message = tkinter.Message(window, textvariable=var1, bg='green', fg='white')
+            message.pack()
+        else:
+            var1 = tkinter.StringVar()
+            var1.set("Invalid stock name. Enter a valid stock name")
             message = tkinter.Message(window, textvariable=var1, bg='green', fg='white')
             message.pack()
 
@@ -162,5 +217,5 @@ def gui_function():
     button2 = tkinter.Button(frame, text='Worst Six Months', bg='red', fg='white')
     button2.bind('<Button>', display_worst)
     button2.pack()
-
     window.mainloop()
+
--- test_mining.py
+++ test_mining.py
@@ -1,36 +1,52 @@
 #!/usr/bin/env python3
 
-""" Docstring """
+""" Module to test mining.py """
 
-__author__ = 'Susan Sim'
-__email__ = "ses@drsusansim.org"
+__author__ = 'Anisha/Grant/Vidhya'
 
-__copyright__ = "2014 Susan Sim"
-__license__ = "MIT License"
-
-__status__ = "Prototype"
-
-# imports one per line
+import pytest
 from mining import *
 
 
 def test_goog():
+    # test for GOOG json file
+    # function call to read stock data and calculate monthly average stock prices
     read_stock_data("GOOG", "data/GOOG.json")
+    # test for best six months
     assert six_best_months() == [('2007/12', 693.76), ('2007/11', 676.55), ('2007/10', 637.38), ('2008/01', 599.42),
                                  ('2008/05', 576.29), ('2008/06', 555.34)]
+    # test for worst six months
     assert six_worst_months() == [('2004/08', 104.66), ('2004/09', 116.38), ('2004/10', 164.52), ('2004/11', 177.09),
                                   ('2004/12', 181.01), ('2005/03', 181.18)]
+    # test for visualization
     visualize("GOOG")
 
 
 def test_tse_so():
+    # test for TSE-SO json file
+    # function call to read stock data and calculate monthly average stock prices
     read_stock_data("TSE-SO", "data/TSE-SO.json")
-    assert six_best_months() == [('2007/12', 693.76), ('2007/11', 676.55), ('2007/10', 637.38), ('2008/01', 599.42),
-                                 ('2008/05', 576.29), ('2008/06', 555.34)]
+    # test for six best months
+    assert six_best_months() == [('2007/12', 20.98), ('2007/11', 20.89), ('2013/05', 19.96), ('2013/06', 19.94),
+                                 ('2013/04', 19.65), ('2007/10', 19.11)]
+    # test for six worst months
     assert six_worst_months() == [('2009/03', 1.74), ('2008/11', 2.08), ('2008/12', 2.25), ('2009/02', 2.41),
                                   ('2009/04', 2.75), ('2009/01', 3.14)]
+    # test for visualization
     visualize("TSE-SO")
 
 
 def test_gui():
+    # test for graphical user interface
     gui_function()
+
+
+def test_files():
+    # file not found test cases
+    with pytest.raises(FileNotFoundError):
+        read_stock_data("GOOG", "GOOG.json")
+
+    # invalid stock name test case
+    with pytest.raises(ValueError):
+        read_stock_data("ABC", "abc.json")
+        read_stock_data("", "goog.json")
